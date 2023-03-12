0b4dd1660a6ca26e9baaa682f8983ad51f51cd18
Mamdouh Adel <mamdouh.elmallah@yahoo.com>
2023-03-12 16:56:33 +0200

    Next 06/11

--- TestApiJWT/Program.cs
+++ TestApiJWT/Program.cs
@@ -1,13 +1,19 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using TestApiJWT.Data;
 using TestApiJWT.Helpers;
 using TestApiJWT.Models;
+using TestApiJWT.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // JWT Configuration
-builder.Configuration.GetValue<JWT>("JWT");
+JWT jwt = builder.Configuration.GetValue<JWT>("JWT");
+
+builder.Services.AddScoped<IAuthService, AuthService>();
 
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 
@@ -22,17 +28,39 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+}).AddJwtBearer(o =>
+{
+    o.RequireHttpsMetadata = false;
+    o.SaveToken = false;
+    o.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        ValidateAudience = true,
+        ValidateIssuer = true,
+        ValidateLifetime = true,
+        ValidIssuer = jwt.Issuer,
+        ValidAudience = jwt.Audience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt.Key)),
+    };
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
---
+++ TestApiJWT/Services/AuthService.cs
@@ -0,0 +1,16 @@
+﻿using TestApiJWT.Models;
+
+namespace TestApiJWT.Services
+{
+    public class AuthService : IAuthService
+    {
+        public AuthService()
+        {
+        }
+
+        public Task<AuthModel> RegisterAsync(RegisterModel registerModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
